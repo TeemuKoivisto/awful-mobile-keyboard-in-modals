8af19a50593a83cc890fbed05dbee790878c7cac
Teemu Koivisto <tkoivisto456@gmail.com>
2024-10-13 21:48:11 +0300

    edit text

--- packages/client/src/modals/MeltUI-v2.svelte
+++ packages/client/src/modals/MeltUI-v2.svelte
@@ -15,7 +15,7 @@
 </script>
 
 <div class="flex flex-col">
-  <a class="hover:underline" href="https://melt-ui.com/docs/builders/dialog">Source</a>
+  <p>Based on V3</p>
   <button class="rounded px-4 py-1 bg-gray-200 hover:bg-gray-300" use:melt={$trigger}>
     melt-ui v2
   </button>
